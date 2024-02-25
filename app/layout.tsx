64d1387ae106784c9a98bd460e2334cdb56b1d9c
jasinskiadam <38188852+jasinskiadam@users.noreply.github.com>
2024-02-25 20:41:32 +0100

    feat: themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Sidebar } from '@/components/sidebar'
+import { ThemeProvider } from '@/components/theme-provider'
+import { ModeToggle } from '@/components/theme-toggle'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,21 +21,32 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <div className=' border-b'>
-          <div className='flex h-16 items-center px-4'>
-            Prezentytu
-            <div className='ml-auto flex items-center space-x-4'>
-              <Avatar>
-                <AvatarImage src='https://github.com/shadcn.png' />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className=' border-b'>
+            <div className='flex h-16 items-center px-4'>
+              Prezentytu
+              <div className='ml-auto flex items-center space-x-4'>
+                <ModeToggle />
+                <Avatar>
+                  <AvatarImage
+                    alt='avatar image'
+                    src='https://github.com/shadcn.png'
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='flex'>
-          <Sidebar />
-          <div className='w-full px-4 py-2'>{children}</div>
-        </div>
+          <div className='flex'>
+            <Sidebar />
+            <div className='w-full px-4 py-2'>{children}</div>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   )
